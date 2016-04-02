af14530127213e3cd988458e3fb132211e28cd73
George Hunt <georgejhunt@gmail.com>
2016-04-01 20:20:48 -0700

    add grub2-mkconfig to sysprep

--- roles/installer/files/oncentos.sh
+++ roles/installer/files/oncentos.sh
@@ -18,11 +18,4 @@ cd xsce
 ./runtags download,download2
 ./install-console
 
-# create a grub config file that uses devices rather than UUIDs
-grep UUID /etc/default/grub
-if [ $? -ne 0 ]; then
-  echo GRUB_DISABLE_UUID=true >> /etc/default/grub
-fi
-grub2-mkconfig -o /boot/grub/grub.cfg
-
 echo "all done"
